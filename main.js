be95f7c1320cc95b6d23f7a6b27e30badb86f1c6
Stanley Ke <kestanley101@gmail.com>
2024-07-01 12:33:21 -0400

    updates

--- main.js
+++ main.js
@@ -1,48 +1,50 @@
 
+document.addEventListener('DOMContentLoaded', () => {
+    // NAVBAR RELATED
+    {
+        const sidebar = document.querySelector('.sidebar');
+        
+        function revealSidebar() {
+            sidebar.classList.add('active');
+        }
+        
+        function closeSidebar() {
+            sidebar.classList.remove('active');
+        }
 
-// NAVBAR RELATED
-{
-    const sidebar = document.querySelector('.sidebar');
-    
-    function revealSidebar() {
-        sidebar.classList.add('active');
-    }
-    
-    function closeSidebar() {
-        sidebar.classList.remove('active');
-    }
+        const menuIcon = document.querySelector('#menu-icon');
+        const menuIconSvg = document.querySelector('#menu-icon-svg');
+        menuIcon.addEventListener("mouseover", ()=> {
+            console.log('hovering');
+            menuIcon.classList.add('hovering');
+            menuIconSvg.setAttribute('fill', "#EC5D53");
 
-    const menuIcon = document.querySelector('#menu-icon');
-    const menuIconSvg = document.querySelector('#menu-icon-svg');
-    menuIcon.addEventListener("mouseover", ()=> {
-        console.log('hovering');
-        menuIcon.classList.add('hovering');
-        menuIconSvg.setAttribute('fill', "#EC5D53");
-
-    });
-
-    menuIcon.addEventListener("mouseout", ()=> {
-        console.log('not hovering');
-        menuIcon.classList.remove('hovering');
-        menuIconSvg.setAttribute('fill', "black");
-    });
-
-}
-
-// ABOUT RELATED
-{
-    const socialIcons = document.querySelectorAll('.social-icons li');
-    socialIcons.forEach(icon => {
-        icon.addEventListener("click", () => {
-            if (icon.classList.contains('active')){
-                icon.classList.remove('active');
-            }
-            else {
-                socialIcons.forEach(i => i.classList.remove('active'));
-                // Add 'active' class to the clicked icon
-                icon.classList.add('active');
-            }
-            
         });
-    });
-}
+
+        menuIcon.addEventListener("mouseout", ()=> {
+            console.log('not hovering');
+            menuIcon.classList.remove('hovering');
+            menuIconSvg.setAttribute('fill', "black");
+        });
+
+    }
+
+    // HOMEPAGE RELATED
+    {
+        const socialIcons = document.querySelectorAll('.social-icons li');
+        socialIcons.forEach(icon => {
+            icon.addEventListener("click", () => {
+                if (icon.classList.contains('active')){
+                    icon.classList.remove('active');
+                }
+                else {
+                    socialIcons.forEach(i => i.classList.remove('active'));
+                    // Add 'active' class to the clicked icon
+                    icon.classList.add('active');
+                }
+                
+            });
+        });
+    }
+});
+
